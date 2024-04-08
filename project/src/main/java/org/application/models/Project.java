967864c414c369ad3ab001a18d7ea96af64db5aa
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-04-08 16:08:35 +0200

    Add methods/fields to Project.java, ReservedActivity.java, TimeBlock.java

--- project/src/main/java/org/application/models/Project.java
+++ project/src/main/java/org/application/models/Project.java
@@ -1,4 +1,25 @@
 package org.application.Models;
 
 public class Project {
+    String name;
+    int projectID;
+    Date endWeek;
+    Date startWeek;
+    String projectLeaderID;
+
+    public Report createReport(){
+
+    }
+
+    public addActivity(Activity activity){
+
+    }
+
+    public deleteActivity(Activity activity){
+
+    }
+
+    public assignProjectLeader(String projectLeaderID){
+
+    }
 }
