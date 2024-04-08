6f0ea7a01d2e75b672b75e8e15723f335764035b
manajuDK <manaju.dk@gmail.com>
2024-04-08 15:51:33 +0200

    test environment set-up

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.application {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens org.application.Controllers to javafx.fxml;
     exports org.application;
 }
---
+++ project/src/test/java/org/test/ActivitySteps.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ActivitySteps {
+    @Then("An activity is created")
+    public void anActivityIsCreated() {
+        // Write code here that turns the phrase above into concrete actions    
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The activity ends in week {int}")
+    public void theActivityEndsInWeek(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions    
+        throw new io.cucumber.java.PendingException();
+    }
+}
