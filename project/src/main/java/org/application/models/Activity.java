2ab035764cfe522b2c269d048b0df2c19964d943
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-04-08 15:48:02 +0200

    Update Activity.java

--- project/src/main/java/org/application/models/Activity.java
+++ project/src/main/java/org/application/models/Activity.java
@@ -1,4 +1,14 @@
 package org.application.models;
 
 public abstract class Activity {
+    String name;
+    public void assignEmployee(String employeeID)
+    {
+        //TODO: Implement this
+    }
+
+    public void removeEmployee(String employeeID)
+    {
+        //TODO: Implement this
+    }
 }
