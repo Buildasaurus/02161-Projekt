cf14a0a998c7a5d54bf462cb9afdbdc80d4d2aec
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-04-08 15:56:07 +0200

    Methods/fields added to ProjectActivity.java

--- project/src/main/java/org/application/models/ProjectActivity.java
+++ project/src/main/java/org/application/models/ProjectActivity.java
@@ -1,4 +1,19 @@
 package org.application.models;
 
 public class ProjectActivity {
+    Date startTime;
+    Date endTime;
+    Date expectedDuration;
+
+    public void addTimeBlock(TimeBlock timeBlock){
+
+    }
+
+    public Time calculateCompletionTime(){
+
+    }
+
+    public void removeTimeBlock(TimeBlock timeBlock){
+        
+    }
 }
