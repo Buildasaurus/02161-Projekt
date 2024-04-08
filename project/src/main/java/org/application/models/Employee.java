7a4753cce8fda9927ec768c7cc745960d39a6919
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-04-08 16:00:35 +0200

    Add methods/fields to Employee.java

--- project/src/main/java/org/application/models/Employee.java
+++ project/src/main/java/org/application/models/Employee.java
@@ -1,4 +1,21 @@
 package org.application.models;
 
 public class Employee {
+    String ID;
+
+    public void addActivity(Activity activity){
+
+    }
+
+    public void removeActivity(Activity activity){
+
+    }
+
+    public void createTimeBlock(Activity activity, Date startDate, Date endDate){
+
+    }
+
+    public void deleteTimeBlock(TimeBlock timeBlock){
+        
+    }
 }
