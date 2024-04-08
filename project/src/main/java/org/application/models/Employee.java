a1c80dac7b376661043dc31a5575bb5ed34225dd
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-04-08 16:02:55 +0200

    Update Employee.java

--- project/src/main/java/org/application/models/Employee.java
+++ project/src/main/java/org/application/models/Employee.java
@@ -4,18 +4,22 @@ public class Employee {
     String ID;
 
     public void addActivity(Activity activity){
+        //TODO: Implement this
 
     }
 
     public void removeActivity(Activity activity){
+        //TODO: Implement this
 
     }
 
     public void createTimeBlock(Activity activity, Date startDate, Date endDate){
+        //TODO: Implement this
 
     }
 
     public void deleteTimeBlock(TimeBlock timeBlock){
-        
+        //TODO: Implement this
+
     }
 }
