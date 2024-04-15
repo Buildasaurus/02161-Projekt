c2ad8030cf066cfaaeda97523766cecaca90196e
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-04-15 11:50:52 +0200

    Update comments

--- project/src/main/java/org/application/Models/System.java
+++ project/src/main/java/org/application/Models/System.java
@@ -8,33 +8,32 @@ public class System { // should be public static class, but java is stupid
     public static List<Project> projects = new ArrayList<Project>();
     public static List<Employee> employees = new ArrayList<>();
 
-    static
-    {
+    static {
         //Loading employees
         employees.add(new Employee("404040"));
 
         // TODO : make this read employees from a file
-    }
-    public static void addEmployee(Employee employee)
-    {
-        employees.add(employee);
-        //TODO : Make this also save the employee to the file
+        //  - This is though an extra feature. Not high priority
     }
 
-    public static void removeEmployee(Employee employee)
-    {
+    public static void addEmployee(Employee employee) {
+        employees.add(employee);
+        //TODO : Make this also save the employee to the file
+        // - This is though an extra feature. Not high priority
+    }
+
+    public static void removeEmployee(Employee employee) {
         employees.remove(employee);
         //TODO : Make this also save the employee to the file
         // TODO : consider if this should have some effect on view? perhaps tell it to update.
     }
 
-    public static List<Employee> findAvailableEmployees()
-    {
+    public static List<Employee> findAvailableEmployees() {
         // TODO Implement this
         return null;
     }
-    public static void createNewProject(Project project)
-    {
+
+    public static void createNewProject(Project project) {
         projects.add(project);
     }
 }
