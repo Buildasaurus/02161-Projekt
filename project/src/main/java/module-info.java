2691d755f8918f27d70342c66d0e2f750d76481b
manajuDK <manaju.dk@gmail.com>
2024-04-15 16:11:22 +0200

    forgot this one :p

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -2,6 +2,5 @@ module org.application {
     requires transitive javafx.controls;
     requires java.desktop;
 
-    opens org.application.Controllers to javafx.fxml;
     exports org.application;
 }
