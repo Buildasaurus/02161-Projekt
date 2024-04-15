92b566dceab94e1a7f92e5890d87527c63be1e0a
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-04-15 11:09:30 +0200

    Semi-implement ProjectActivity

--- project/src/main/java/org/application/models/ProjectActivity.java
+++ project/src/main/java/org/application/models/ProjectActivity.java
@@ -1,15 +1,22 @@
 package org.application.Models;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ProjectActivity {
     Date startTime;
     Date endTime;
     Date expectedDuration;
+    List<TimeBlock> timeBlocks = new ArrayList();
 
+    public ProjectActivity(Date startTime, Date endTime, Date expectedDuration) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.expectedDuration = expectedDuration;
+    }
 
     public void addTimeBlock(TimeBlock timeBlock){
-        //TODO: Implement this
-
+        timeBlocks.add(timeBlock);
     }
 
     /**
@@ -17,13 +24,13 @@ public class ProjectActivity {
      * @return an integer of how many half-hours has passed for this activity
      */
     public int calculateCompletionTime(){
-        //Returns numbers of half
+        //Returns numbers of half hours
         //TODO: Implement this
         return 0;
     }
 
     public void removeTimeBlock(TimeBlock timeBlock){
-        //TODO: Implement this
+        timeBlocks.remove(timeBlock);
 
     }
 }
