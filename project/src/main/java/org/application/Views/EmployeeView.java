37feb69575ca151722873de0a7c86f0a114a4e97
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-04-29 13:23:35 +0200

    Add return to main menu button

--- project/src/main/java/org/application/Views/EmployeeView.java
+++ project/src/main/java/org/application/Views/EmployeeView.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.application.Controllers.EmployeeController;
 import org.application.Models.Activity;
+import org.application.Models.Buttons;
 import org.application.Models.TimeBlock;
 import java.awt.event.ActionEvent;
 
@@ -43,9 +44,6 @@ public class EmployeeView extends VBox {
             ActivityView activityView = new ActivityView(activity);
             getChildren().add(activityView);
         }
-    }
-
-    public void handleLogin(ActionEvent event) {
-        System.out.println("Login successfull");
+        getChildren().add(Buttons.returnButton());
     }
 }
