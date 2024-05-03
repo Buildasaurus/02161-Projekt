a6f3df1259ab281059db8a353f75e3722e5080e0
Martin Mejer <s235469@dtu.dk>
2024-05-03 12:31:46 +0200

    Fix more tests

--- project/src/test/java/org/test/EmployeeSteps.java
+++ project/src/test/java/org/test/EmployeeSteps.java
@@ -1,8 +1,14 @@
 package org.test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.GregorianCalendar;
+
 import org.application.Models.Activity;
 import org.application.Models.Employee;
 import org.application.Models.SystemModel;
+import org.application.Models.TimeBlock;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 //TODO look for duplicate steps that could be consolidated
@@ -23,9 +29,16 @@ public class EmployeeSteps {
 
     @When("the employee spends time on the activity")
     public void spendTimeOnActivity() {
-//        Employee testEmployee = SystemModel.getEmployees().get(0);
-//        Activity testActivity = SystemModel.getActivities().get(0);
-//        testActivity.
+        Employee testEmployee = SystemModel.getEmployees().get(0);
+        Activity testActivity = SystemModel.getActivities().get(0);
+        GregorianCalendar startTime = new GregorianCalendar(1,1,1);
+        GregorianCalendar endTime = new GregorianCalendar(1,1,1);
+        new TimeBlock(startTime, endTime, testActivity, testEmployee);
     }
 
+    @Then("the employee has {int} timeblocks attached")
+    public void employeeHasTimeBlockAttached(int expectedTimeBlocks) {
+        Employee testEmployee = SystemModel.getEmployees().get(0);
+        assertEquals(expectedTimeBlocks,testEmployee.getTimeBlocks().size());
+    }
 }
