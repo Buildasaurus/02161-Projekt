3bb15e6cefdec81383ccd2467cb8df4e8a7ae970
manajuDK <manaju.dk@gmail.com>
2024-05-06 22:36:50 +0200

    Fix duplicate employee IDs allowed

--- project/src/main/java/org/application/Controllers/CreateEmployeeController.java
+++ project/src/main/java/org/application/Controllers/CreateEmployeeController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import org.application.App;
 import org.application.Models.Employee;
+import org.application.Models.SystemModel;
 import org.application.Views.GeneralAlert;
 import org.application.Views.MainView;
 
@@ -22,7 +23,26 @@ public class CreateEmployeeController implements IController {
     }
 
     public void handleCompletePressed(ActionEvent event, String name) {
+        try {
+            if (name.length() > 4) {
+                throw new IllegalArgumentException("ID must be at most 4 characters long");
+            }
+            if (SystemModel.getEmployee(name) != null) {
+                throw new IllegalArgumentException("An employee with this ID already exists");
+            }
+            new Employee(name);
+            MainView newView = new MainView();
+            MainController controller = new MainController(newView);
+            newView.setController(controller);
+            App.setRoot(controller);
+        } catch (Exception exception) {
+            GeneralAlert.sendWarning(exception.getMessage());
+        }
+        /*
         if (name.length() <= 4) {
+            if (SystemModel.getEmployee(name) != null) {
+
+            }
             new Employee(name);
             MainView newView = new MainView();
             MainController controller = new MainController(newView);
@@ -31,5 +51,6 @@ public class CreateEmployeeController implements IController {
         } else {
             GeneralAlert.sendWarning("ID must be at least 4 characters long");
         }
+        */
     }
 }
