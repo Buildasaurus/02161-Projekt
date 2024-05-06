6d816df4040f7ee4faabe2262abdcb58abea5527
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-05-06 15:14:17 +0200

    Test SystemModel get setters

--- project/src/test/java/JUnitTests/GetSetTests.java
+++ project/src/test/java/JUnitTests/GetSetTests.java
@@ -1,7 +1,9 @@
 package JUnitTests;
 
+import org.application.Models.Employee;
 import org.application.Models.Project;
 import org.application.Models.SystemModel;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.GregorianCalendar;
@@ -11,6 +13,12 @@ import static org.junit.Assert.assertNull;
 
 public class GetSetTests
 {
+    //reset before every test
+    @Before
+    public void start() {
+        SystemModel.reset();
+    }
+
     @Test
     public void ProjectSetters() {
         GregorianCalendar start = new GregorianCalendar(1,1,1);
@@ -36,6 +44,23 @@ public class GetSetTests
         GregorianCalendar end = new GregorianCalendar(1,2,1);
         Project project = new Project("My Project", start, end);
         assertNull(SystemModel.getProjectByName("doesNotExist"));
-
     }
+
+    @Test
+    public void SystemModelGetEmployee() {
+        SystemModel.addEmployee(new Employee("me"));
+        SystemModel.addEmployee(new Employee("you"));
+        assertNull(SystemModel.getEmployee("doesNotExist"));
+    }
+
+    @Test
+    public void SystemModelAddProject() {
+        GregorianCalendar start = new GregorianCalendar(1,1,1);
+        GregorianCalendar end = new GregorianCalendar(1,2,1);
+        Project project = new Project("My Project", start, end);
+        SystemModel.addProject(project);
+        assertEquals(1, SystemModel.getProjects().size());
+    }
+
+
 }
